94ea9d419de6e45a9f02ac89135e66934ce76871
Jhon Andrew Q. Baes <anecbook@gmail.com>
2017-08-10 10:41:17 +0800

    Optimizations and fixes.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,9 @@
+/* global angular, google, fetchJsonp, TransportApi */
 (function () {
-  /* global angular, google, fetchJsonp */
+  // The App Module
   let app = angular.module('busDepartureBoard', ['ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages'])
 
+  // The App Controller
   app.controller('main', ($scope, $mdSidenav) => {
     // $mdSidenav | https://material.angularjs.org/latest/demo/sidenav#custom-sidenav
     $scope.toggle = sidenavId => {
@@ -18,96 +20,92 @@
     })
 
     // TransportAPI Credentials
-    let transportApi = {
-      api: 'http://transportapi.com/v3',
-      appId: '681b0dc0',
-      appKey: 'f6c866682eb106c191551aadde2b222b'
-    }
-
-    // Get nearby stops | https://developer.transportapi.com/docs?raml=https://transportapi.com/v3/raml/transportapi.raml##uk_bus_stops_near_json
-    let getNearbyStops = (page = 1, lat = defaultLocation.lat, lng = defaultLocation.lng) => {
-      let endpoint = `${transportApi.api}/uk/bus/stops/near.json?lat=${lat}&lon=${lng}&page=${page}&app_id=${transportApi.appId}&app_key=${transportApi.appKey}`
-      return fetchJsonp(endpoint).then(resp => { return resp.json() })
-    }
-
-    // Get bus departure | https://developer.transportapi.com/docs?raml=https://transportapi.com/v3/raml/transportapi.raml##uk_bus_stop_atcocode_live_json
-    let getBusDeparture = atcocode => {
-      let endpoint = `${transportApi.api}/uk/bus/stop/${atcocode}/live.json?group=route&app_id=${transportApi.appId}&app_key=${transportApi.appKey}`
-      return fetchJsonp(endpoint).then(resp => { return resp.json() })
-    }
+    let transportApi = new TransportApi({
+      id: '681b0dc0',
+      key: 'f6c866682eb106c191551aadde2b222b'
+    })
 
     // Bus Stops for the Sidenav
     $scope.busStops = []
 
-    // getNearbyStops by page (max to 25 results per page)
-    let loadPage = (page = 1) => getNearbyStops(page).then(data => {
-      let { stops } = data
+    // How many pages to load multiplied by 25 is the number of markers to be created.
+    let pagesToLoad = 40
+    let loadPage = 1
 
-      // Bus Stops
-      stops.forEach(stop => {
-        let { atcocode } = stop
+    let loadPages = () => {
+      if (loadPage < pagesToLoad) {
+        transportApi.getNearbyStops(loadPage).then(data => {
+          let { stops } = data
 
-        // Create marker
-        let marker = new google.maps.Marker({
-          position: { lat: stop.latitude, lng: stop.longitude },
-          map: map,
-          title: stop.stop_name
-          // icon: 'https://url.to/your.img'
-        })
+          // Bus Stops
+          stops.forEach(stop => {
+            let { atcocode } = stop
 
-        // After clicking a marker
-        marker.addListener('click', () => {
-          // Show a loading status while fetching data...
-          let loader = new google.maps.InfoWindow({
-            content: `
-              <div class="map-info">
-                <h3 class="md-healing">${stop.stop_name}</h3>
-                <p>Loading...</p>
-              </div>
-            `
-          })
+            // Create marker
+            let marker = new google.maps.Marker({
+              position: { lat: stop.latitude, lng: stop.longitude },
+              map: map,
+              title: stop.stop_name
+              // icon: 'https://url.to/your.img'
+            })
 
-          loader.open(map, marker)
+            // After clicking a marker
+            marker.addListener('click', () => {
+              // Show a loading status while fetching data...
+              let loader = new google.maps.InfoWindow({
+                content: `
+                  <div class="map-info">
+                    <h3 class="md-healing">${stop.stop_name}</h3>
+                    <p>Loading...</p>
+                  </div>
+                `
+              })
 
-          getBusDeparture(atcocode).then(data => {
-            let { departures } = data
-            let departureTemplate = []
+              loader.open(map, marker)
 
-            Object.keys(departures).forEach(key => {
-              departures[key].forEach(bus => {
-                // Add Bus operator, bus line number, & departure estimate
-                departureTemplate.push(`<span>${bus.operator} ${bus.line}: <strong>${bus.best_departure_estimate}</strong></span>`)
+              transportApi.getBusDeparture(atcocode).then(data => {
+                let { departures } = data
+                let departureTemplate = []
+
+                Object.keys(departures).forEach(key => {
+                  departures[key].forEach(bus => {
+                    // Add Bus operator, bus line number, & departure estimate
+                    departureTemplate.push(`<span>${bus.operator} ${bus.line}: <strong>${bus.best_departure_estimate}</strong></span>`)
+                  })
+                })
+
+                // Create the information template
+                let infoCard = new google.maps.InfoWindow({
+                  content: `
+                    <div class="map-info">
+                      <h3 class="md-heading">${stop.stop_name}</h3>
+                      <p>${departureTemplate.join('<br />')}</p>
+                    </div>
+                  `
+                })
+
+                loader.close() // Close loader
+                infoCard.open(map, marker) // Open information template
               })
             })
 
-            // Create the information template
-            let infoCard = new google.maps.InfoWindow({
-              content: `
-                <div class="map-info">
-                  <h3 class="md-heading">${stop.stop_name}</h3>
-                  <p>${departureTemplate.join('<br />')}</p>
-                </div>
-              `
-            })
+            // Create a trigger function to open marker from sidenav
+            stop.trigger = () => {
+              $scope.toggle('sideNavigation')
+              let Trigger = google.maps.event.trigger
+              return new Trigger(marker, 'click')
+            }
 
-            loader.close() // Close loader
-            infoCard.open(map, marker) // Open information template
+            $scope.busStops.push(stop)
           })
+
+          loadPage++
+          loadPages()
         })
+      } else console.log('All pages should now be loaded.')
+    }
 
-        // Create a trigger function to open marker from sidenav
-        stop.trigger = () => {
-          $scope.toggle('sideNavigation')
-          let Trigger = google.maps.event.trigger
-          return new Trigger(marker, 'click')
-        }
-
-        $scope.busStops.push(stop)
-      })
-    }) // --> getNearbyStops
-
-    // How many pages to load multiplied by 25 is the number of markers to be created.
-    let pagesToLoad = 40
-    for (let i = 1; i <= pagesToLoad; i++) loadPage(i)
+    // Start loading pages
+    loadPages()
   }) // --> app.controller
 }())
